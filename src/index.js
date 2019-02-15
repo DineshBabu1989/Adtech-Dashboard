37d44f7a411e7f41839fd38777bd1c633ddd3f59
Shruthi Shri AS <shruthishrias@Shruthis-Air.local>
2019-02-15 12:46:34 +0100

    front architecture, folder structures complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = <h1>Architecture setup for front end complete</h1>;
+
+ReactDOM.render(App, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
